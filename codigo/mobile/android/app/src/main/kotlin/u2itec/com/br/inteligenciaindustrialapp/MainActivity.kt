174817db874ff6eb47febde0456fd705d0376324
Samuel Baker <samuelbaker@tangerino.com.br>
2021-04-02 22:03:41 -0300

    login and register screen

---
+++ codigo/mobile/android/app/src/main/kotlin/u2itec/com/br/inteligenciaindustrialapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package u2itec.com.br.inteligenciaindustrialapp
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
